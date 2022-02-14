a385f35eebf063b2197166dfee5828e2836f98e3
a-sakhizada <arezousakhizada@gmail.com>
2022-02-13 19:00:33 -0500

    made api call to get coordinates based on ip address

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //will run and require our main fetch function
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
 fetchMyIP((error, ip) => {
 
@@ -9,4 +9,12 @@ fetchMyIP((error, ip) => {
     }
 
     console.log("it worked! Returned IP: ", ip);
+});
+
+fetchCoordsByIP("162.2445.144.188", (error, coordinates) => {
+  if (error) {
+    console.log("it didnt work!", error);
+    return;
+  }
+  console.log("it worked! Returned coordinate: ", coordinates);
 });
--- iss.js
+++ iss.js
@@ -38,4 +38,43 @@ const fetchMyIP = function (callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
+
+    //   let data = {
+    //     latitude: JSON.parse(body).latitude,
+    //     longitude: JSON.parse(body).longitude,
+    //   };
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
